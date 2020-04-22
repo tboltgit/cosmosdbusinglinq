f95b3c2d26a094b94da4cffb0bb02ac51b8eeaef
vishwakarma.m.amit@outlook.com <amitvishwakarma@amitvish.localdomain>
2020-04-22 13:07:26 +0530

    initial commit

---
+++ DocumentDB.Models/VedioGame.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DocumentDB.Model
+{
+    public class VedioGame
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("releaseDate")]
+        public string ReleaseDate { get; set; }
+        [JsonProperty("engine")]
+        public string Engine { get; set; }
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+    }
+}
